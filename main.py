70623ea0d3216bcd88ef831c7cbd527101b39d60
mokpohans9029 <mokpohans9029@gmail.com>
2021-11-17 17:30:43 +0900

    로그인 창에서 PW부분 '*'으로 가리고 ssh서버의 결과 값 가져와 출력하는 시간 단축

--- main.py
+++ main.py
@@ -5,18 +5,19 @@ from tkinter import filedialog
 from tkinter import ttk
 from tkinter import messagebox
 
-global cli, sshcmdline, cmd, sshcmd
+global cli,sshcmdline, cmd, sshcmd, channel,output
 
 
-def ssh_login(IP, Port, ID, PW):
+def ssh_login():
+    global cli,channel
     #ssh서버에 접속하기(로그인) 위한 함수
     try:
-        global cli
         # paramiko에서 SSHClient 객체를 cli에 생성
         cli = paramiko.SSHClient()
         cli.set_missing_host_key_policy(paramiko.AutoAddPolicy)
         print(IP.get(), Port.get(), ID.get(), PW.get())
         cli.connect(IP.get(), port=Port.get(), username=ID.get(), password=PW.get())
+        channel = cli.invoke_shell()
         print("ssh connect")
         ssh_cmdwin()
 
@@ -25,22 +26,24 @@ def ssh_login(IP, Port, ID, PW):
 
 
 def ssh_command():
-    global cli, sshcmdline, cmd, sshcmd
-    channel = cli.invoke_shell()
+    global sshcmdline, cmd, sshcmd, channel,output
+
     sshcmds = cmd.get() + "\n"
-    print(sshcmds)
     # 명령 송신
     channel.send(sshcmds)
-    time.sleep(1.0)
+    time.sleep(0.5)
     # 결과 수신
+    ssh_print()
+
+def ssh_print():
+    global output,sshcmdline, cmd, sshcmd, channel
     output = channel.recv(65535).decode("utf-8")
     sshcmdline.insert('end', output)
     sshcmd.delete("0", "end")
     sshcmdline.see('end')
     print(output)
 
-
-def ssh_cmdwin(win):
+def ssh_cmdwin():
     global cmd, sshcmdline, sshcmd
 
     ssh = tk.Toplevel(win)
@@ -59,7 +62,7 @@ def ssh_cmdwin(win):
     sshcmd = ttk.Entry(ssh, textvariable=cmd)
     sshcmd.pack(side='bottom', fill='x')
     sshcmd.bind("<Return>", lambda event: ssh_command())
-
+    ssh_print()
 
 def files_upload():
     #파일을 선택하고 ssh서버에 업로드하는 함수
@@ -82,7 +85,7 @@ def files_upload():
         print(err)
 
 
-def on_closing(win):
+def on_closing():
     if messagebox.askokcancel("Quit", "Do you want to quit?"):
         try:
             cli.close()
@@ -118,11 +121,11 @@ ID_entry.grid(row=4, column=1, columnspan=2, sticky=tk.W)
 ttk.Label(user_frame, text="PW :").grid(row=5, column=0, sticky=tk.W)
 
 PW = tk.StringVar()
-PW_entry = ttk.Entry(user_frame, textvariable=PW)
+PW_entry = ttk.Entry(user_frame, textvariable=PW,show='*')
 PW_entry.grid(row=5, column=2, sticky=tk.W)
 
-ttk.Button(win, text='Connect', command=ssh_login(IP, Port, ID, PW)).grid(row=6, column=0, sticky=tk.E)
+ttk.Button(win, text='Connect', command=ssh_login).grid(row=6, column=0, sticky=tk.E)
 
-win.protocol("WM_DELETE_WINDOW", on_closing(win))
+win.protocol("WM_DELETE_WINDOW", on_closing)
 win.mainloop()
 
